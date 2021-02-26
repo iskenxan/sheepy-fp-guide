160fa99c502e8fcaf526c3097f42952910ece00c
Iskander Samatov <iskenxan11@gmail.com>
2021-02-25 19:59:28 -0600

    cleanup initial template

---
+++ hands-on-functional-programming/build/precache-manifest.89950d6163ad755dbd05888327b41924.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "670c49e5ab5a1b80ad42",
+    "url": "/static/css/main.646f9e1f.css"
+  },
+  {
+    "revision": "670c49e5ab5a1b80ad42",
+    "url": "/static/js/main.670c49e5.js"
+  },
+  {
+    "revision": "ce4ea3e9703a7153706127e99c538454",
+    "url": "/index.html"
+  }
+];
--- hands-on-functional-programming/build/service-worker.js
+++ hands-on-functional-programming/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e3f9ce449e977f643ea18594577863ac.js"
+  "/precache-manifest.89950d6163ad755dbd05888327b41924.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
