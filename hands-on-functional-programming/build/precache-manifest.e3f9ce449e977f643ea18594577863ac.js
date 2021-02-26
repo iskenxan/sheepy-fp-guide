ad4042de12e6a7a310d587da1d28492f92e4096d
Iskander Samatov <iskenxan11@gmail.com>
2021-02-25 19:44:08 -0600

    add react template

---
+++ hands-on-functional-programming/build/precache-manifest.e3f9ce449e977f643ea18594577863ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "685ca29e07089a37f234",
+    "url": "./static/css/main.8d7b873b.css"
+  },
+  {
+    "revision": "685ca29e07089a37f234",
+    "url": "./static/js/main.685ca29e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f78223707a457185d136418cb45a196a",
+    "url": "./index.html"
+  }
+];
