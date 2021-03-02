a26bbf1a1d68d8f617d576caf2f270bfed9caa86
Iskander Samatov <iskenxan11@gmail.com>
2021-03-02 17:49:38 -0600

    rebuild

---
+++ hands-on-functional-programming/build/precache-manifest.5a2f3bc57fa1bcb4b033b2bc086e49cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61f7d2a035826752ebb0",
+    "url": "./static/css/main.62c6af0a.css"
+  },
+  {
+    "revision": "61f7d2a035826752ebb0",
+    "url": "./static/js/main.61f7d2a0.js"
+  },
+  {
+    "revision": "5e720f0f4bf122a7a467db4217435a45",
+    "url": "./static/media/sheepy.5e720f0f.png"
+  },
+  {
+    "revision": "017d495f9aa73ccc33bc2aaaa93b9aa2",
+    "url": "./static/media/ramda.017d495f.png"
+  },
+  {
+    "revision": "d213121d5a569f2be7119e3a11940241",
+    "url": "./index.html"
+  }
+];
