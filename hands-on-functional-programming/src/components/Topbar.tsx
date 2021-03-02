b53d57ccc90496585a432873c40945ee7673780b
Iskander Samatov <iskenxan11@gmail.com>
2021-03-02 17:49:10 -0600

    add ramda js reference

--- hands-on-functional-programming/src/components/Topbar.tsx
+++ hands-on-functional-programming/src/components/Topbar.tsx
@@ -1,12 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, color, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/sheepy.png";
+import ramda_logo from "../assets/ramda.png";
+import { colors } from "../utils/theme";
 
 export default function Topbar() {
   return (
-    <Box w="100%" height="60px" d="flex">
-      <Box height="40px" marginRight="auto">
-        <img src={logo} />
+    <Box
+      w="100%"
+      marginBottom="32px"
+      alignItems="center"
+      height="60px"
+      d="flex"
+    >
+      <Box height="40px">
+        <Image src={logo} />
+      </Box>
+      <Box ml="12px" d="flex" marginRight="auto">
+        <Text color={colors.fgColor} fontSize="12px">
+          Interactive FP guide for Ramda.js
+        </Text>
+        <Image marginLeft="8px" h="12px" src={ramda_logo} />
       </Box>
     </Box>
   );
